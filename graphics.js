c6db5d582403aa667ba06353c6dd75a854081459
Yoory <yoory.nagumanov@gmail.com>
2012-09-28 13:01:19 +0700

    Add canvas element.

--- graphics.js
+++ graphics.js
@@ -1,1 +1,32 @@
 
+var canvas;
+var context;
+
+
+/**
+ * Инициализируем приложение.
+ */
+function init()
+{
+	canvas = document.getElementById('view');
+
+	if (!canvas)
+	{
+		alert('Error: элемент canvas не найден!');
+		return;
+	}
+	else if (!canvas.getContext)
+	{
+		alert('Error: нет метода canvas.getContext!');
+		return;
+	}
+
+	context = canvas.getContext('2d');
+
+	if (!context)
+	{
+		alert('Error: не удалось получить контекст!');
+		return;
+	}
+}
+
